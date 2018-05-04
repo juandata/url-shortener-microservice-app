da367086c35ccd1f36ab08aa5f91a2707caefd21
Glitch (url-shortener-microservice-app) <none>
2018-05-04 19:19:04 +0000

    🌯🚟 Checkpoint ./package.json:1033623/51 ./server.js:1033623/884

--- server.js
+++ server.js
@@ -1,5 +1,18 @@
 // server.js
 // where your node app starts
+//lets require/import the mongodb native drivers.
+var mongodb = require('mongodb');
+
+//We need to work with "MongoClient" interface in order to connect to a mongodb server.
+var MongoClient = mongodb.MongoClient;
+
+// Connection URL. This is where your mongodb server is running.
+
+//(Focus on This Variable)
+var urlRemoteEnvVariable= process.env.MONGOLAB_URI;
+console.log(urlRemoteEnvVariable);
+console.log("esto");
+
 /*
 // init project
 var express = require('express');
@@ -21,4 +34,17 @@ var listener = app.listen(process.env.PORT, function () {
   console.log('Your app is listening on port ' + listener.address().port);
 });
 */
+/*
+MongoClient.connect(urlRemoteEnvVariable, function (err, db) {
+if (err) {
+  console.log('Unable to connect to the mongoDB server. Error:', err);
+} else {
+  console.log('Connection established to mlab.com:15340/urlshortened');
 
+  // do some work here with the database.
+
+
+  //Close connection
+  db.close();
+}
+});*/
