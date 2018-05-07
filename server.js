664319b8eb4d93f11635f89120ab8d67f8b6f23f
Glitch (url-shortener-microservice-app) <none>
2018-05-07 22:01:44 +0000

    ✍️👻 Checkpoint ./server.js:1033623/6005 ./views/index.html:1033623/144

--- server.js
+++ server.js
@@ -15,107 +15,76 @@ var address = process.env.SECRET;
 var jsonObject = {};
 
 
-//we've started you off with Express, 
-//but feel free to use whatever libs or frameworks you'd like through `package.json`.
-
-// http://expressjs.com/en/starter/static-files.html
+//we've started you off with Express,
 app.use(express.static('public'));
-
-// http://expressjs.com/en/starter/basic-routing.html
-app.get("/", function (req, res) {
+app.get("/", function(req, res) {
   res.sendFile(__dirname + '/views/index.html');
-  //res.redirect(url);
 });
-app.get(/\d/, function (req, response, next)  {
+app.get(/\d/, function(req, response, next) {
   let fullUrl = req.protocol + '://' + req.get('host') + req.originalUrl;
 
-   MongoClient.connect(address, function (err, db) {
-   //(Focus on This Variable)
-if (err) {
-  console.log('Unable to connect to the mongoDB server. Error:', err);
-} else {
-  console.log('Connection established to mlab.com');
-  // do some work here with the database.
-  var  dbo = db.db("urlshortened");
-  dbo.collection('urls').find({ short_url: fullUrl}).limit(1).next(function(err, doc){
-  if(!doc) {
-    response.send("the url does not exist in the database, try again with a correct number");
-  }
-    else {
-      console.log("redirecting...");
-      response.redirect(doc.original_url);
+  MongoClient.connect(address, function(err, db) {
+    //(Focus on This Variable)
+    if (err) {
+      console.log('Unable to connect to the mongoDB server. Error:', err);
+    } else {
+      console.log('Connection established to mlab.com');
+      // do some work here with the database.
+      var dbo = db.db("urlshortened");
+      dbo.collection('urls').find({
+        short_url: fullUrl
+      }).limit(1).next(function(err, doc) {
+        if (!doc) {
+          response.send("the url does not exist in the database, try again with a correct number");
+        } else {
+          console.log("redirecting...");
+          response.redirect(doc.original_url);
+        }
+      });
+      //Close connection
+      db.close();
+      //});
     }
   });
-  //Close connection
-  db.close();
-//});
-} 
 });
-});
-app.use('/new', function (req, res){
+app.use('/new', function(req, res) {
   var url = req.url.substr(1);
   var number = Math.floor((Math.random() * 10000) + 1);
   let fullUrl = req.protocol + '://' + req.get('host') + '/' + number;
-  console.log( url, fullUrl);
+  console.log(url, fullUrl);
   writeToDatabase(url, fullUrl);
   var body = {
     "original_url": url,
     "short_url": fullUrl
   }
-    res.json(body);
-
+  res.json(body);
 });
 // listen for requests :)
-var listener = app.listen(process.env.PORT, function () {
+var listener = app.listen(process.env.PORT, function() {
   console.log('Your app is listening on port ' + listener.address().port);
 });
 
-function writeToDatabase(url, fullUrl){
-  MongoClient.connect(address, function (err, db) {
-   //(Focus on This Variable)
-if (err) {
-  console.log('Unable to connect to the mongoDB server. Error:', err);
-} else {
-  console.log('Connection established to mlab.com');
+function writeToDatabase(url, fullUrl) {
+  MongoClient.connect(address, function(err, db) {
+    //(Focus on This Variable)
+    if (err) {
+      console.log('Unable to connect to the mongoDB server. Error:', err);
+    } else {
+      console.log('Connection established to mlab.com');
 
-  // do some work here with the database.
-  var jsonObject = {
-  original_url : url,
-  short_url : fullUrl
-}, dbo = db.db("urlshortened");
-dbo.collection('urls').insert( jsonObject, function(err, ok){
-  if (err) throw err;
-  if (ok) console.log('document inserted!', ok);
-});
+      // do some work here with the database.
+      var jsonObject = {
+          original_url: url,
+          short_url: fullUrl
+        },
+        dbo = db.db("urlshortened");
+      dbo.collection('urls').insert(jsonObject, function(err, ok) {
+        if (err) throw err;
+        if (ok) console.log('document inserted!', ok);
+      });
 
-  //Close connection
-  db.close();
+      //Close connection
+      db.close();
+    }
+  });
 }
-});
-}
-/*
-function  searchOnDatabase(fullUrl) {
-     MongoClient.connect(address, function (err, db) {
-   //(Focus on This Variable)
-if (err) {
-  console.log('Unable to connect to the mongoDB server. Error:', err);
-} else {
-  console.log('Connection established to mlab.com');
-  // do some work here with the database.
-  var  dbo = db.db("urlshortened");
-  dbo.collection('urls').find({ short_url: fullUrl})
-.toArray(function(err, res){
-  if (err) throw err;
-  jsonObject = {
-  original_url : res[0].original_url,
-  short_url : res[0].short_url
-}
- console.log(jsonObject);
-});
-  //Close connection
-  db.close();
-} 
-});
-  
-  }
-*/
