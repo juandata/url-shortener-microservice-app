ef24cfcaaee0478696db43e48b5f5a4c04f25a09
Glitch (url-shortener-microservice-app) <none>
2018-05-07 15:55:06 +0000

    🌵🐴 Checkpoint ./server.js:1033623/609 ./views/index.html:1033623/89

--- server.js
+++ server.js
@@ -23,6 +23,7 @@ app.use(express.static('public'));
 // http://expressjs.com/en/starter/basic-routing.html
 app.get("/", function (request, response) {
   response.sendFile(__dirname + '/views/index.html');
+  connectToDatabase();
 
 });
 
@@ -31,6 +32,29 @@ var listener = app.listen(process.env.PORT, function () {
   console.log('Your app is listening on port ' + listener.address().port);
 });
 
+function connectToDatabase(){
+  MongoClient.connect(address, function (err, db) {
+   //(Focus on This Variable)
+if (err) {
+  console.log('Unable to connect to the mongoDB server. Error:', err);
+} else {
+  console.log('Connection established to mlab.com');
+
+  // do some work here with the database.
+  var jsonObject = {
+  name : "Juan David",
+  lastName : "Tabares Arce"
+}, dbo = db.db("urlshortened");
+dbo.collection('users4').insert( jsonObject, function(err, ok){
+  if (err) throw err;
+  if (ok) console.log('document inserted!', ok);
+});
+
+  //Close connection
+  db.close();
+}
+});
+}
 /*
 MongoClient.connect(address, function (err, db) {
    //(Focus on This Variable)
